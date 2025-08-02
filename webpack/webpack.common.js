e7d2bb9e4058a1e992c9c75a1dbe82aed6666ef1
navi-cc <157870465+navi-cc@users.noreply.github.com>
2025-08-02 22:39:41 +0800

    Add html-loader to module.rules

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -19,6 +19,11 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      }
     ],
   },
 };
