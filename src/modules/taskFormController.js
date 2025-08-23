f706dac9679123d7c1b722fae24574847fe3963d
navi-cc <157870465+navi-cc@users.noreply.github.com>
2025-08-23 08:48:33 +0800

    Modify the showForm method

--- src/modules/taskFormController.js
+++ src/modules/taskFormController.js
@@ -24,7 +24,7 @@ export default class TaskFormController {
     })
 
     static showForm() {
-        TaskFormController.changeSubmitButtonState(1)
+        TaskFormController.changeFormState(1)
         ModalController.makeModalActive()
     }
 
